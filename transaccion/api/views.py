5509a1e98c2e09f977eb377c25f5a03bfdb6bdc2
nicolas-labasse <nicolaslabasse4@gmail.com>
2024-03-18 12:18:19 -0300

    viste webhook

--- transaccion/api/views.py
+++ transaccion/api/views.py
@@ -1,14 +1,11 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from transaccion.api.serializers import TransaccionSerializer
-from transaccion.models import Transaccion
 
 
 class TransaccionApiViewSet(ModelViewSet):
-    serializer_class = TransaccionSerializer
-    queryset = Transaccion.objects.all()
-    def create(self, request, *args, **kwargs):
+    print('vista del webhook')
+    def get(self, request, *args, **kwargs):
         data = request.data
-        print("Datos recibidos en el webhook:", data)
-        return Response()
+        print("Datos recibidos en la vista:", data)
+        return Response(data)
