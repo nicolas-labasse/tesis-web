a0d0ba0ad3c70e23e4f7270507dc6c7707cd9831
nicolas-labasse <nicolaslabasse4@gmail.com>
2024-03-20 17:05:44 -0300

    push

--- transaccion/api/views.py
+++ transaccion/api/views.py
@@ -14,12 +14,13 @@ class TransaccionApiViewSet(ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         data = request.data
-        mp_id = data.get('collection_id')
+        mp_id = data.get('id')
         usuario_id = 1
+        precio = data['external_reference']
         
         if mp_id and usuario_id:
             usuario = get_object_or_404(Usuario, id=usuario_id)
-            transaccion = Transaccion.objects.create(mp_id=mp_id, usuario=usuario)
+            transaccion = Transaccion.objects.create(mp_id=mp_id, usuario=usuario, precio=int(precio))
             serializer = self.get_serializer(transaccion)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
