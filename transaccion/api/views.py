a43c02d7ec7311cfb44b8335274052be054c83f4
nicolas-labasse <nicolaslabasse4@gmail.com>
2024-03-20 21:51:13 -0300

    push

--- transaccion/api/views.py
+++ transaccion/api/views.py
@@ -18,14 +18,13 @@ class TransaccionApiViewSet(ModelViewSet):
         data = request.data
         mp_id = data.get('id')
         usuario_id = 1
-        precio = data.get('external_reference')
 
 
         if mp_id and usuario_id:
             usuario = get_object_or_404(Usuario, id=usuario_id)
-            transaccion = Transaccion.objects.create(mp_id=mp_id, usuario=usuario, precio=int(precio))
+            transaccion = Transaccion.objects.create(mp_id=mp_id, usuario=usuario)
             serializer = self.get_serializer(transaccion)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(f"Data: {json_data}",serializer.data, status=status.HTTP_201_CREATED)
         else:
             json_data = JSONRenderer().render(data)
             return Response(f"Error: Se requiere ID de transacción y ID de usuario. Data: {json_data}", status=status.HTTP_400_BAD_REQUEST)
